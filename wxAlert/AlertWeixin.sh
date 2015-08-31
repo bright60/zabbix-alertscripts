ce2e0d94070edd3edc00d2f7d888158e316c124b
hulei <hulei@T4F-MBP-14209.local>
2015-08-31 22:52:33 +0800

    =init

---
+++ wxAlert/AlertWeixin.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+user="$1"
+subject="$2"
+content="$3"
+
+useropenid=$user
+eventtime=$(echo "$content" |grep "Event tim" |sed 's#Event time : ## ')
+itemvalue=$(echo "$content" |tail -3 |head -1 |sed 's#1. ##')
+
+touser=$useropenid
+first="$subject"
+keyword1=""
+keyword2=$eventtime
+keyword3=$itemvalue
+remark=""
+
+
+
+cd /usr/lib/zabbix/alertscripts/wxAlert/
+
+python ./wxAlert.py "$touser" "$first" "$keyword1" "$keyword2" "$keyword3" "$remark" 2>&1 |\
+  grep -i -v insecureplatformwarning >> ../logs/wxAlert.log 
